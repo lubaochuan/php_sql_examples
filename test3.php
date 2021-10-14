2682fd46da15df37cb063014c93d3fa9d3849ff6
Baochuan Lu <blu@sbuniv.edu>
2021-10-14 09:00:45 -0500

    add another complete example

--- test3.php
+++ test3.php
@@ -1,12 +1,34 @@
 <?php
 
-$db = new PDO("mysql:dbname=imdb", "webuser", "password");
-$rows = $db->query("SELECT * FROM actors WHERE last_name LIKE 'Del%'");
-foreach ($rows as $row) {
-  ?>
-  <li> First name: <?= $row["first_name"] ?>,
-       Last name:  <?= $row["last_name"]  ?> </li>
-  <?php
+$servername = "localhost";
+$dbname = "imdb";
+$username = "webuser";
+$password = "password";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+$sql = "SELECT * FROM actors WHERE last_name LIKE 'Del%'";
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+?>
+    <li>
+    First name: <?=$row["first_name"]?>, Last name:  <?=$row["last_name"]?>
+    </li>
+<?php
+  }
+} else {
+  echo "0 results";
 }
 
 ?>
