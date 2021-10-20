d0abb0e601b07084739988bffa905f61c11d1d11
Baochuan Lu <blu@sbuniv.edu>
2021-10-20 11:45:40 -0500

    add query example

--- test.php
+++ test.php
@@ -2,8 +2,12 @@
 
 $db = new PDO("mysql:dbname=imdb", "webuser", "password");
 $rows = $db->query("SELECT * FROM actors WHERE last_name LIKE 'Del%'");
+#print_r($rows);
 foreach ($rows as $row) {
   ?>
+<?php
+print_r($row);
+?>
   <li> First name: <?= $row["first_name"] ?>,
        Last name:  <?= $row["last_name"]  ?> </li>
   <?php
--- test2.php
+++ test2.php
@@ -14,4 +14,22 @@ if (!$conn) {
 }
 echo "Connected successfully";
 
+$first = "Tom";
+$last = "Cruise";
+
+$sql =
+"SELECT name, year FROM actors, movies, roles".
+" WHERE actors.id = roles.actor_id".
+" AND roles.movie_id = movies.id".
+" AND first_name = '".$first."'".
+" AND last_name = '".$last."'";
+
+echo $sql;
+
+$result = mysqli_query($conn, $sql);
+
+while($row = mysqli_fetch_assoc($result)) {
+  print_r($row);
+  print("</br>");
+}
 ?>
